15ce73c0bc75d70cbc086ff2292c7e329aec1bd9
mahfuzh129 <mahfuzh266@gmail.com>
2025-12-17 16:48:12 +0600

    added a new route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const admin = require('firebase-admin')
 const port = process.env.PORT || 3000
 const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf-8')
@@ -14,11 +15,7 @@ const app = express()
 // middleware
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'https://b12-m11-session.web.app',
-    ],
+    origin:[process.env.CLIENT_DOMAIN],
     credentials: true,
     optionSuccessStatus: 200,
   })
@@ -59,20 +56,20 @@ async function run() {
     const decoratorsCollection = db.collection('decorators')
 
     // sevices from db
-    app.get('/services',async(req,res)=>{
+    app.get('/services', async (req, res) => {
 
       const result = await servicesCollection.find().limit(6).toArray()
       res.send(result)
 
     })
-    app.get('/services-all',async(req,res)=>{
+    app.get('/services-all', async (req, res) => {
 
       const result = await servicesCollection.find().toArray()
       res.send(result)
 
     })
 
-     app.get('/services/:id', async (req, res) => {
+    app.get('/services/:id', async (req, res) => {
       const id = req.params.id
       const result = await servicesCollection.findOne({ _id: new ObjectId(id) })
       res.send(result)
@@ -80,7 +77,7 @@ async function run() {
     })
 
     // decorators from db
-     app.get('/top-decorators',async(req,res)=>{
+    app.get('/top-decorators', async (req, res) => {
 
       const result = await decoratorsCollection.find().toArray()
       res.send(result)
@@ -88,7 +85,7 @@ async function run() {
     })
 
 
-     // save or update a user in db
+    // save or update a user in db
     app.post('/user', async (req, res) => {
       const userData = req.body
       userData.created_at = new Date().toISOString()
@@ -118,6 +115,36 @@ async function run() {
     })
 
 
+    // Payment endpoints
+    app.post('/create-checkout-session', async (req, res) => {
+      const paymentInfo = req.body
+      console.log(paymentInfo)
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: 'bdt',
+              product_data: {
+                name: paymentInfo?.name,
+                description: paymentInfo?.description,
+                images: [paymentInfo.image],
+              },
+              unit_amount: paymentInfo?.price * 100,
+            },
+            quantity: paymentInfo?.quantity,
+          },
+        ],
+       customer_email: paymentInfo?.customer?.email,
+        mode: 'payment',
+        metadata: {
+          serviceId: paymentInfo?.serviceId,
+          customer: paymentInfo?.customer.email,
+        },
+        success_url: `${process.env.CLIENT_DOMAIN}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.CLIENT_DOMAIN}/service/${paymentInfo?.serviceId}`,
+      })
+      res.send({ url: session.url })
+    })
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
